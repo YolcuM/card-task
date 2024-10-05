25e15a5ad465c6bd02bcd1d5bf7f7ba2063a009c
YolcuM <maryam.pana@gmail.com>
2024-10-06 01:08:43 +0300

    -

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card } from "./Card.jsx";
+
+
+
+
+const cards = [
+  {
+    id: 1,
+    title: "Безлимитный 300",
+    price: 300,
+    speed: 10,
+  },
+  {
+    id: 2,
+    title: "Безлимитный 450",
+    price: 450,
+    speed: 50,
+  },
+  {
+    id: 3,
+    title: "Безлимитный 550",
+    price: 550,
+    speed: 100,
+  },
+  {
+    id: 4,
+    title: "Безлимитный 1000",
+    price: 1000,
+    speed: 200,
+  },
+
+]
 
 function App() {
-  return (
+function getColor (index) {
+  switch (index) {
+    case 0:
+      return "green";
+    case 1:
+      return "pink";
+    case 2:
+      return "purple";
+    case 3:
+      return "grey";
+    default:
+      return "blue";
+  }
+}
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+{cards.map((card,index)=>{
+  return <Card color={getColor(index)} card={card}/>;
+
+})}
     </div>
   );
 }
